f5263341fcea792dfb48366450800b7667b83447
nkiyotaka <60053095+nkiyotaka@users.noreply.github.com>
2020-02-06 00:22:58 +0900

    新規作成

---
+++ YCNo908.java
@@ -0,0 +1,39 @@
+/*
+javac -encoding UTF-8 YCNo908.java
+java YCNo908
+*/
+import java.util.*;
+import java.util.regex.Pattern;
+public class YCNo908{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
+        try{
+        	String line = sc.nextLine();
+        	int loop = line.length();
+        	char work;
+        	for(int i = 0 ; i < loop ; i++){
+        		work = line.charAt(i);
+        		if(i % 2 == 0 && !regexp(Character.toString(work))){
+        			flag = false;
+        		}
+        		if(i % 2 == 1 && !(work == ' ')){
+        			flag = false;
+        		}
+        	}
+        	System.out.println(flag ? "YES" : "NO");
+        }catch(Exception e){
+        	;
+        }finally{
+        	sc.close();
+        }
+    }
+    /**
+	 * 英小小文字判定（正規表現）
+	 * @param 文字（1文字）
+	 * @return true／false
+	 */
+    private static boolean regexp(String work){
+    	return Pattern.matches("^[a-z]+$", work);
+    }
+}
