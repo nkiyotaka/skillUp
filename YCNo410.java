a78db4a651aa38604d75f37012378bb97875c868
kiyotaka <kiy0tak@icloud.com>
2020-02-19 21:20:58 +0900

    新規作成

---
+++ YCNo410.java
@@ -0,0 +1,65 @@
+/*
+javac -encoding UTF-8 YCNo410.java
+java YCNo410
+*/
+import java.util.*;
+
+public class YCNo410{
+	public static void main(String[] args){
+		try(Scanner sc = new Scanner(System.in)){
+			int[] A = coordinatePoint(sc);
+			int[] B = coordinatePoint(sc);
+			int Ax_By = calculation(A[0],B[1]);
+			int Ay_Bx = calculation(A[1],B[0]);
+			result(Ax_By+Ay_Bx);
+		}catch(Exception e){
+			;
+		}
+	}
+	/**
+	* 整数座標点取得
+	* @param スキャナー変数
+	* @return
+	*/
+	private static int[] coordinatePoint(Scanner sc){
+		String[] num = null;
+		boolean flag = true;
+		while(flag){
+			num = sc.nextLine().split(" ");
+			flag = decisionInRange(num);
+		}
+		int[] xy = {Integer.valueOf(num[0]),Integer.valueOf(num[1])};
+		return xy;
+	}
+	/**
+	* 整数座標点範囲判定
+	* @param 整数座標点
+	* @return
+	*/
+	private static boolean decisionInRange(String[] num){
+		for(String str : num){
+			if( !(Integer.valueOf(str) >= -100000 && Integer.valueOf(str) <= 100000) ){
+				return true;
+			}
+		}
+		return false;
+	}
+	/**
+	* 計算判定
+	* @param x軸、y軸
+	* @return
+	*/
+	private static int calculation(int x,int y){
+		if(x < 0 && y < 0){return -x + -y;}
+		return x + y;
+	}
+	/**
+	* 結果表示
+	* @param xy軸
+	* @return
+	*/
+	private static void result(int xy){
+		if(xy % 2 == 0){System.out.println(xy / 2);}
+		else{System.out.println((double)xy / 2);}
+	}
+}
